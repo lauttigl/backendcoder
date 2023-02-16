e7c5faf5ddafe6f812cfd51be130172f7e762faa
lauttigl <ljiglesias@outlook.com>
2023-02-16 20:16:27 -0300

    primer desafio

--- primerdesafio.js
+++ primerdesafio.js
@@ -10,43 +10,50 @@ class ProductManager{
 	};
 
 	createProduct = (title,description,price,stock,thumbnail,code) => {
-		const product = {
-			code: this.products.length +1,
-			title,
-			description,
-			price,
-			stock,
-			thumbnail, 
-		};
-
-		this.products.push(product);
+		if (!title || !description || !price || !stock || !thumbnail || !code) {
+			console.log("Error: all fields are mandatory");
+			return;
+	
 	};
-	//HAY QUE REVISAR ESTA FUNCION Y REEMPLAZAR ADDPRODUCT POR GETPRODUCTBYID (VER EN LA DIAPOSITIVA )
-	// addProduct = (productCode, productId) => {
-	// 	const productIndex = this.products.findIndex((product) => product.code === productCode)
-
-	// 	if (productIndex === -1) {
-	// 		console.log("Not Found")
-	// 		return;
-	// 	}
-		
-	// 	const productExists = this.products[productIndex].code.includes(productId)
-
-	// 	if (productExists) {
-	// 		console.log("The product already exists")
-	// 		return;
-	// 	}
-
-	// 	this.products[productIndex].code.push(productId)
-	// };
-
-
+	const productIndex = this.products.findIndex((product) => product.code === code);
+    if (productIndex !== -1) {
+      console.log("Error: code already exists");
+      return;
+    }
+	const product = {
+		id: this.products.length + 1,
+		code,
+		title,
+		description,
+		price,
+		stock,
+		thumbnail,
+	  };
+  
+	  this.products.push(product);
+	  console.log("Product added successfully");
+};
+getProductById = (productId) => {
+    const product = this.products.find((prod) => prod.id === productId);
+    if (product) {
+      console.log("Product found");
+      console.log(product);
+    } else {
+      console.log("Error: Product not found");
+    }
+  };
 }
+
+
 
 const productManager = new ProductManager()
 
 
-productManager.createProduct("TV Led Curvo","Una TV de 60'' ",100000,50, "url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.muycomputer.com%2F2016%2F01%2F20%2Ftelevisores-pantalla-curva-ventajas%2F&psig=AOvVaw3HU0RlpiZLlaZzucqsqEY-&ust=1676587086933000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLjcguHLmP0CFQAAAAAdAAAAABAI)")
-productManager.createProduct("Microondas","Microondas Samsung ",50000,30, "url(foto)")
+productManager.createProduct("TV Led Curvo","Una TV de 60'' ",100000,50, "url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.muycomputer.com%2F2016%2F01%2F20%2Ftelevisores-pantalla-curva-ventajas%2F&psig=AOvVaw3HU0RlpiZLlaZzucqsqEY-&ust=1676587086933000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLjcguHLmP0CFQAAAAAdAAAAABAI)", 1)
+productManager.createProduct("Microondas","Microondas Samsung ",50000,30, "url(foto)", 2)
+productManager.createProduct("Otro producto","Otra descripcion ",150,20, "url(foto)", 3)
+
+productManager.getProductById(1)
+productManager.getProductById(4) // Error: Product not found
 
 productManager.getProducts();
